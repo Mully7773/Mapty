4b73cd8af85e05f487171b84e589399db8a5d4f9
Nick M <mully7773@gmail.com>
2022-07-02 14:30:25 -0500

    Refactored code to include classes instead of only functions

--- script.js
+++ script.js
@@ -11,108 +11,115 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
-//getCurrentPosition takes two callback functions: the first is the function that will be called to get the users coordinates (user selects 'allow'), the second will be called if there is an error (user selects 'block')
-if (navigator.geolocation)
-  //to get pop-up window asking for location
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      console.log(position);
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(latitude, longitude);
-      //not working...
-      //   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-      //test for Japan:
-      //   console.log(
-      //     `https://www.google.com/maps/search/?api=1&query=36.2048,138.2529`
-      //   );
-      //working link - not 100% accurate
-      console.log(
-        `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
+// let map, mapEvent;
+
+class App {
+  #map;
+  #mapEvent;
+  //constructor method is called immediately after a new object is created from the App class right when the page is loaded - instead of writing app._getPosition outside the class, we can do it like this in the constructor
+  constructor() {
+    this._getPosition();
+    //have to bind this because we need it to point to the class App object itself not to the form
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    //we don't need to bind() this callback function because we don't actually use the this keyword within the function - see _toggleElevationField
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    //getCurrentPosition takes two callback functions: the first is the function that will be called to get the users coordinates (user selects 'allow'), the second will be called if there is an error (user selects 'block')
+    if (navigator.geolocation)
+      //to get pop-up window asking for location
+      //must bind to get this keyword
+      navigator.geolocation.getCurrentPosition(
+        //bind(this) points to current object
+        //!normally we bind to an object name (in this case, it would be app, but app is outside of the scope of this function, so we must use 'this' to refer to the new instance of App (called 'app') created by the App Class)
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
       );
+  }
 
-      const coords = [latitude, longitude];
+  _loadMap(position) {
+    console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(latitude, longitude);
+    //test for Japan:
+    //   console.log(
+    //     `https://www.google.com/maps/search/?api=1&query=36.2048,138.2529`
+    //   );
+    //working link - not 100% accurate
+    console.log(
+      `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
+    );
 
-      //second argument is how much zoom you want
-      map = L.map('map').setView(coords, 12);
-      //   console.log(map);
+    const coords = [latitude, longitude];
 
-      //For openstreetmap:
-      //   L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      //     attribution:
-      //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      //   }).addTo(map);
+    //second argument is how much zoom you want
+    this.#map = L.map('map').setView(coords, 12);
+    //   console.log(map);
 
-      //For Google Maps:
-      L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-      }).addTo(map);
+    //For openstreetmap:
+    //   L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    //     attribution:
+    //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    //   }).addTo(map);
 
-      //Handling clicks on map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus(); //focuses on distance
+    //For Google Maps:
+    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    }).addTo(this.#map);
 
-        console.log(mapEvent);
-        //grab lat and lng from mapEvent.latlng
-        // const { lat, lng } = mapEvent.latlng;
-        // //use [lat, lng] instead of coords
-        // L.marker([lat, lng])
-        //   .addTo(map)
-        //   .bindPopup(
-        //     //See Leaflet docs for options
-        //     L.popup({
-        //       maxWidth: 250,
-        //       minWidth: 100,
-        //       autoClose: false,
-        //       closeOnClick: false,
-        //       className: 'running-popup',
-        //     })
-        //   )
-        //   .setPopupContent('Workout')
-        //   .openPopup();
-      });
-    },
-    function () {
-      alert('Could not get your position');
-    }
-  );
+    //Handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus(); //focuses on distance
 
-  //Clear input fields:
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-  // Display the marker
-  // grab lat and lng from mapEvent.latlng
-  const { lat, lng } = mapEvent.latlng;
-  //use [lat, lng] instead of coords
-  L.marker([lat, lng], {
-    riseOnHover: true,
-  })
-    .addTo(map)
-    .bindPopup(
-      //See Leaflet docs for options
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
+    console.log(this.#mapEvent);
+  }
 
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    //Clear input fields:
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    // Display the marker
+    // grab lat and lng from mapEvent.latlng
+    const { lat, lng } = this.#mapEvent.latlng;
+    //use [lat, lng] instead of coords
+    L.marker([lat, lng], {
+      riseOnHover: true,
+    })
+      .addTo(this.#map)
+      .bindPopup(
+        //See Leaflet docs for options
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
+
+const app = new App();
